54e034f32c65aa8da897211a33517fc6f2b4ccaf
xabz <117483033+abzalliance@users.noreply.github.com>
2025-01-01 15:04:13 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Функція для виведення роздільної лінії
+function printDelimiter {
+  echo "==========================================="
+}
+
+# Функція для виведення тексту фіолетовим кольором з жирним шрифтом
+function printPurple {
+  echo -e "\e[1m\e[35m${1}\e[0m"
+}
+
+# Функція для відображення логотипу
+function printLogo {
+  bash <(curl -s "https://raw.githubusercontent.com/abzalliance/logo/master/logo.sh")
+}
 
